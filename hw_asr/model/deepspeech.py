fd8444b3c3c2e0c40ae0e887ef16cdc8a02d889e
mac-mvak <maximusev1290@gmail.com>
2023-10-20 23:14:39 +0000

    Add Conv2d

--- hw_asr/model/deepspeech.py
+++ hw_asr/model/deepspeech.py
@@ -13,22 +13,38 @@ class DeepSpeech(BaseModel):
         self.convs_params = convs_params
         self.grus_len = len(grus_params)
         self.convs = nn.Sequential()
+        size_after_feat = n_feats
         for conv_params in self.convs_params:
-            self.convs.append(nn.Conv1d(**conv_params['convolution']))
-            self.convs.append(nn.BatchNorm1d(**conv_params['batch_norm']))
+            if self.conv_type == "Conv1d":
+                self.convs.append(nn.Conv1d(**conv_params['convolution']))
+                self.convs.append(nn.BatchNorm1d(**conv_params['batch_norm']))
+                size_after_feat = conv_params['convolution']["out_channels"]
+            elif self.conv_type == "Conv2d":
+                self.convs.append(nn.Conv2d(**conv_params['convolution']))
+                self.convs.append(nn.BatchNorm2d(**conv_params['batch_norm']))
+                size_after_feat = _conv_shape_transform(size_after_feat, dim=0, **conv_params['convolution'])
             self.convs.append(nn.ReLU())
         self.grus = nn.ModuleList()
         self.bnorms = nn.ModuleList()
-        for gru_params in grus_params:
-            self.grus.append(nn.GRU(**gru_params['gru']))
+        if self.conv_type == "Conv2d":
+            size_after_feat *= self.convs_params[-1]['convolution']["out_channels"]
+        for i, gru_params in enumerate(grus_params):
+            if i != 0:
+                self.grus.append(nn.GRU(**gru_params['gru']))
+            else:
+                self.grus.append(nn.GRU(input_size = size_after_feat, **gru_params['gru']))
             self.bnorms.append(nn.BatchNorm1d(**gru_params['batch_norm']))
         self.fc = nn.Linear(in_features=2*grus_params[-1]['gru']['hidden_size'], 
                             out_features=n_class)
 
 
     def forward(self, spectrogram, **batch):
+        if self.conv_type == "Conv2d":
+            spectrogram = spectrogram.unsqueeze(1)
         conv_spec = self.convs(spectrogram)
-        out = conv_spec
+        if self.conv_type == "Conv2d":
+            out = torch.permute(conv_spec, (0, 3, 1, 2))
+            out = out.reshape(*out.shape[:2], -1).transpose(1, 2)
         for i, tup in enumerate(zip(self.grus, self.bnorms)):
             gru, bnorm = tup
             out, _ = gru(out.transpose(1, 2))
@@ -39,7 +55,8 @@ class DeepSpeech(BaseModel):
 
     def transform_input_lengths(self, input_lengths):
         for conv_params in self.convs_params:
-            input_lengths = _conv_shape_transform(input_lengths, **conv_params['convolution'])
+            input_lengths = _conv_shape_transform(input_lengths, **conv_params['convolution'], 
+                                                  dim=None if self.conv_type == "Conv1d" else 1)
         return input_lengths 
 
 
--- hw_asr/model/utils.py
+++ hw_asr/model/utils.py
@@ -2,10 +2,16 @@ import torch
 
 
 def _conv_shape_transform(L_in, kernel_size,
-                           padding=0, dilation=1, stride=1, **batch):
+                            padding=torch.tensor([0,0]),
+                            dilation=torch.tensor([1, 1]), 
+                            stride=torch.tensor([1, 1]), dim=None, **batch):
+    if dim is not None:
+        kernel_size = kernel_size[dim]
+        padding = padding[dim]
+        dilation = dilation[dim]
+        stride = stride[dim]
     ans = torch.floor((L_in + 2 * padding - dilation * (kernel_size - 1)-1)/stride + 1).int()
     return ans
-
 
 
 
